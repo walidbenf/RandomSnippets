f4ded661c9e99b9cd631a013534a05b72f18893f
walidbenf <isior@outlook.fr>
2025-02-27 23:19:48 +0100

    FizzBuzz

---
+++ python/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz() :
+	for i in range(0, 100):
+		if (i % 3 == 0 and i % 5 == 0):
+			print('FizzBuzz')
+		elif (i % 3 == 0):
+			print('Fizz')
+		elif (i % 5 == 0):
+			print('Buzz')
+		else :
+			print (i)
+def main():
+	fizzbuzz()
+main()
